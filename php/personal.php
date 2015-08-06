5c3a7eabf54b27ccbdc304eb5127f7f260ac8c72
GabrielD <gdedi651@gmail.com>
2015-08-06 19:16:24 -0400

    add code that gathers user input from form and sends email

---
+++ php/personal.php
@@ -0,0 +1,22 @@
+<?php
+    if(isset($_POST["submit"]))
+    {
+        $to = "gdedi001@fiu.edu";
+        $subject = "Personal Website Inquiry";
+        
+        // Information pulled from the form element in index.html
+        $from = $_POST["fullname"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $message = $_POST["message"];
+        
+        $body = "From: $name\n
+                 E-mail: $email\n
+                 Phone: $phone\n
+                 Message: $message";
+        
+        mail($to, $subject, $body); // PHP mail() function allows you to send emails directly from a script.
+        
+        include("submitted.html");
+    }
+?>
