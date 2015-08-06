ab53546f23f071de330b2b6a60a51998ed6759f3
GabrielD <gdedi651@gmail.com>
2015-08-06 16:42:57 -0400

    once mouseleave event is triggered, rotate (animaton) class will be removed. Good for reuse without reloading webpage

--- js/app.js
+++ js/app.js
@@ -12,6 +12,11 @@ function magnify() {
 function minify() {
                         // Takes 350 milliseconds to complete
     $(this).find("i").animate({fontSize: "30px"}, 350);
+    
+    if ($("i").closest("div").hasClass("checked"))
+    {
+        $(this).find(".checked").removeClass("rotater");
+    }
 }
 // Once the DOM has been loaded, we will listen for the DOM "I'm ready" event.
 $(document).ready(function() {
