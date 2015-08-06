62b2b6125ddd2da123193ffc8242a32154611974
GabrielD <gdedi651@gmail.com>
2015-08-06 16:26:24 -0400

    add code to increase font-size on mouseenter and vice versa for mouseleave

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Create maginify function
+function magnify() {
+                        // Takes 300 milliseconds to complete
+    $(this).find("i").animate({fontSize: "50px"}, 300);
+    if ($("i").hasClass("checked"))
+    {
+        $(this).addClass("rotater");
+    }
+}
+// Create minify function 
+function minify() {
+                        // Takes 350 milliseconds to complete
+    $(this).find("i").animate({fontSize: "30px"}, 350);
+}
+// Once the DOM has been loaded, we will listen for the DOM "I'm ready" event.
+$(document).ready(function() {
+    $(".well").on("mouseenter", magnify);
+    $(".well").on("mouseleave", minify);
+});
